f46d942b960372a6566025d2afee12550d190d31
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-08-13 21:16:48 -0400

    .

--- src/lang-fountain.ts
+++ src/lang-fountain.ts
@@ -564,25 +564,26 @@ class DialogueParser implements LeafBlockParser {
 				}
 			} else if(parseCharacter(this.leaf2.content, this.pos, this.leaf2.start)) {
 				this.changeType(CurrentBlock.Character)
-			} else if(!(cx.prevNode[0] == Type.BoneYard || cx.prevNode[0] == Type.SceneHeading)) {
+			} else if((cx.prevNode[0] == Type.BoneYard || cx.prevNode[0] == Type.SceneHeading)) {
 				let blip = cx.parser.parseInline(line.text, cx.lineStart)
 				cx.addLeafElement(
 					this.leaf2,
 					elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length, blip)
 				)
 				this.changeType(CurrentBlock.Dialogue)
+				return true
 			}
 				// cx.addNode(Type.Dialogue, cx.lineStart)
 				// cx.addElement(elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length + 1, cx.parser.parseInline(line.text, cx.lineStart)))
 
-			/* if(cx.prevNode[1] == Type.Dialogue && cx.prevNode[0] == Type.Action) {
+			if(cx.prevNode[1] == Type.Dialogue && cx.prevNode[0] == Type.Action) {
 				let blip = cx.parser.parseInline(line.text, cx.lineStart)
 				cx.addLeafElement(
 					this.leaf2,
 					elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length, blip)
 				)
 				return true
-			} */
+			}
 			return false
 		} else if(this.current == CurrentBlock.Character) {
 			if(this.nextPart(parseParenthetical(line.text, this.pos, cx.lineStart))) {
