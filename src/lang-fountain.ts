4895f7c4d8e7546e5c27795cf0b2ed17135b47b7
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-08-13 20:08:00 -0400

    *sigh*

--- src/lang-fountain.ts
+++ src/lang-fountain.ts
@@ -285,8 +285,8 @@ function isBlockquote(line: Line) {
 		: 1;
 }
 function isBoneYard(line: Line) {
-	return (line.next == "/".charCodeAt(0) || line.next == "*".charCodeAt(0)) && line.text.charCodeAt(line.pos + 1) == 42
-		? 2
+	return (line.next == 47 || line.next == 42) && (line.text.charCodeAt(line.pos + 1) == 42 || line.text.charCodeAt(line.pos + 1) == 47)
+		? 1
 		: -1
 
 }
@@ -419,7 +419,7 @@ const DefaultBlockParsers: {
 		if(size < 0) return false;
 		cx.startContext(Type.BoneYard, cx.lineStart + line.pos,
 			cx.lineStart + line.text.length)
-		line.moveBase(line.pos + size)
+		line.moveBase(line.pos + 1)
 		return null
 	},
 	// Character(cx, line) {
@@ -587,26 +587,18 @@ class DialogueParser implements LeafBlockParser {
 					// this.elts.push(elt(Type.Parenthetical, this.pos + cx.lineStart, line.text.length + cx.lineStart))
 					// cx.addNode(Type.Parenthetical, cx.lineStart)
 				}
-			} else {
-				if(parseCharacter(this.leaf2.content, this.pos, this.leaf2.start)) {
-					this.changeType(CurrentBlock.Character)
-				} else {
-					let blip = cx.parser.parseInline(line.text, cx.lineStart)
-					cx.addLeafElement(
-						this.leaf2,
-						elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length, blip)
-					)
-					this.changeType(CurrentBlock.Dialogue)
-				}
-				// cx.addNode(Type.Dialogue, cx.lineStart)
-				// cx.addElement(elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length + 1, cx.parser.parseInline(line.text, cx.lineStart)))
-				this.changeType(CurrentBlock.Dialogue)
+			} else if(parseCharacter(this.leaf2.content, this.pos, this.leaf2.start)) {
+				this.changeType(CurrentBlock.Character)
+			} else if(!(cx.prevNode[0] == Type.BoneYard || cx.prevNode[0] == Type.SceneHeading)) {
 				let blip = cx.parser.parseInline(line.text, cx.lineStart)
 				cx.addLeafElement(
 					this.leaf2,
 					elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length, blip)
 				)
+				this.changeType(CurrentBlock.Dialogue)
 			}
+				// cx.addNode(Type.Dialogue, cx.lineStart)
+				// cx.addElement(elt(Type.Dialogue, cx.lineStart, cx.lineStart + line.text.length + 1, cx.parser.parseInline(line.text, cx.lineStart)))
 
 			if(cx.prevNode[1] == Type.Dialogue && cx.prevNode[0] == Type.Action) {
 				let blip = cx.parser.parseInline(line.text, cx.lineStart)
