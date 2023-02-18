e0cc7aa15c0bd16212d686d8c856553746a29332
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-02-18 18:10:11 -0500

    i

--- post-compile.ps1
+++ post-compile.ps1
@@ -1,6 +1,6 @@
 $output="$env:USERPROFILE\projs\obsidian-dev\test-vault"
 $pdir="$output\.obsidian\plugins\obsidian-fountain-revived\"
-$output2="$env:USERPROFILE\Desktop\ART\_tablets-stuff\.obsidian\plugins\obsidian-fountain-revived\"
+$output2="$env:USERPROFILE\Desktop\ART\@tablets-stuff\.obsidian\plugins\obsidian-fountain-revived\"
 Copy-Item main.js $pdir
 Copy-Item .\manifest.json $pdir
 Copy-Item .\src\styles.css $pdir
