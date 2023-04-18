f168ce7ee4ebe43a5b685079feaec75cb4c41a71
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-04-18 16:12:36 -0400

    clean up imports

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,26 +1,20 @@
-import { EditorState, Extension } from "@codemirror/state";
+import { Extension } from "@codemirror/state";
 import {
   EditorView,
-  drawSelection,
   dropCursor,
-  highlightActiveLine,
-  highlightActiveLineGutter,
   highlightSpecialChars,
   keymap,
-  lineNumbers,
-  rectangularSelection,
 } from "@codemirror/view";
 import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from "@codemirror/autocomplete";
 import {
   bracketMatching,
-  defaultHighlightStyle,
+  codeFolding,
   foldGutter,
   foldKeymap,
   indentOnInput,
-  syntaxHighlighting,
 } from "@codemirror/language";
 import { defaultKeymap, history, historyKeymap, indentWithTab } from "@codemirror/commands";
-import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
+import { searchKeymap } from "@codemirror/search";
 
 import { lintKeymap } from "@codemirror/lint";
 
@@ -59,8 +53,6 @@ export const basicSetup: Extension[] = [
   dropCursor(),
 //   EditorState.allowMultipleSelections.of(true),
   indentOnInput(),
-//   fountain(),
-//   syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
   EditorView.lineWrapping,
   bracketMatching(),
   closeBrackets(),
