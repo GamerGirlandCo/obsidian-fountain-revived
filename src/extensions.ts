a53c29a6fd383dccc29e3e2faf0a7c2d6c286bef
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-04-18 16:12:14 -0400

    add custom fold widgets

--- src/extensions.ts
+++ src/extensions.ts
@@ -29,10 +29,33 @@ export const basicSetup: Extension[] = [
   // highlightActiveLineGutter(),
   highlightSpecialChars(),
   history(),
-  foldGutter(),
-//   drawSelection({
-// 	drawRangeCursor: false
-// }),
+  foldGutter({
+    markerDOM(open) {
+      let el = document.createElement("div")
+      el.addClass("screenplay-fold-gutter-sym")
+      if(open) {
+        el.addClass("fold-open")
+        el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>`
+      } else {
+        el.addClass("fold-closed")
+        el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>`
+      }
+      return el;
+    }  
+  }),
+  codeFolding({
+    placeholderDOM(view, onclick) {
+      console.log("pleec", view)
+        const el = document.createElement("span")
+        el.addClass("screenplay-foldwidget")
+        el.onclick = (e) => {
+          onclick(e)
+          console.log("pleec", view)
+        };
+        el.setAttribute("aria-label", view.state.phrase("folded scene (click to unfold)"))
+        return el;
+    },
+  }),
   dropCursor(),
 //   EditorState.allowMultipleSelections.of(true),
   indentOnInput(),
