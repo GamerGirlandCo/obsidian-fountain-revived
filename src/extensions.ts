b8606475b4d53dc821ac4fe1c4a1e09cbb2d56bb
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2023-04-19 13:01:17 -0400

    fold widget tweaks

--- src/extensions.ts
+++ src/extensions.ts
@@ -41,7 +41,7 @@ export const basicSetup: Extension[] = [
     placeholderDOM(view, onclick) {
       console.log("pleec", view)
         const el = document.createElement("span")
-        el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-horizontal"><polyline points="18 8 22 12 18 16"></polyline><polyline points="6 8 2 12 6 16"></polyline><line x1="2" x2="22" y1="12" y2="12"></line></svg>`
+        el.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-move-vertical"><polyline points="8 18 12 22 16 18"></polyline><polyline points="8 6 12 2 16 6"></polyline><line x1="12" x2="12" y1="2" y2="22"></line></svg>`
         el.addClass("screenplay-foldwidget")
         el.onclick = (e) => {
           onclick(e)
